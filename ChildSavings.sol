6fe68995fc06e869f21e05ffcf871dfd8a75ad48
rahman <rahman821.ar@gmail.com>
2024-12-27 21:08:29 +0530

    Child savings initial commit

---
+++ ChildSavings.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract ChildSavings{
+    // owner 
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // define kid
+
+    // add kid to contract
+
+    // deposit funds to contract, specifically to a kid's account
+
+    // kid checks if able to withdraw
+
+    // withdraw money
+}
